9ed96e01d31c919c5d2bf248c52de28eeab9fa33
domene96 <domene96@gmail.com>
2019-04-13 11:54:52 -0500

    small bootstrap change

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -3,7 +3,6 @@
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
@@ -17,9 +16,9 @@ $phoneos = strip_tags(htmlspecialchars($_POST['phoneos']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'redir@domene96.com';
-$email_subject = "Subscriber:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\n$phoneos\tPhone: $phone\n\nMessage:\n$message";
+$to = "redir@domene96.com";
+$email_subject = "Subscriber:\t$name";
+$email_body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\n\nEmail: $email_address\n\n$phoneos\tPhone: $phone\n\nMessage:\n$message";
 // $headers = "From: noreply@domene96.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 // $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body);
