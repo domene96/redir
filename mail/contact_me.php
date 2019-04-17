e24a9278d37a3f9730ea9ecc6ba2286e67f05886
domene96 <domene96@gmail.ccom>
2019-04-17 12:59:30 -0500

    php small change 2

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,7 +18,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = "redir@domene96.com";
 $email_subject = "Subscriber: $name";
-$email_body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\n\nEmail: $email_address\n\n$phoneos\tPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\n\nEmail: $email_address\n\n$phoneos Phone: $phone\n\nMessage:\n$message";
 // $headers = "From: noreply@domene96.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 // $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body);
